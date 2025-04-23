81bea02f0ccd3ffa4315308c779bc4b734f4190a
Ricardo0919 <rickisierra03@gmail.com>
2025-04-23 01:32:04 -0600

    Hotfix Locale Switcher

--- src/app/[locale]/daniel-contreras/page.tsx
+++ src/app/[locale]/daniel-contreras/page.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import Profile from "@/components/DigitalBusinessCard/Profile";
 import Contact from "@/components/DigitalBusinessCard/Contact";
 import DaniCImage from "@/assets/images/DigitalBusinessCard/DaniC.jpg";
+import ButtonLanguage from "@/components/ButtonLanguage";
 
 export default function DanielC() {
     return (
         <div className="bg-gradient-to-br from-lyratech-purple via-[#3f416e] to-lyratech-blue pb-20">
             <Profile imageSrc={DaniCImage} name="Daniel Contreras Chávez"/>
+            <ButtonLanguage />
             <Contact
                 phone="524426142904"
                 email="danicontreras0409@gmail.com"
--- src/app/[locale]/daniel-queijeiro/page.tsx
+++ src/app/[locale]/daniel-queijeiro/page.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import Profile from "@/components/DigitalBusinessCard/Profile";
 import Contact from "@/components/DigitalBusinessCard/Contact";
 import DaniQImage from "@/assets/images/DigitalBusinessCard/DaniQ.jpg";
+import ButtonLanguage from "@/components/ButtonLanguage";
 
 export default function DanielQ() {
     return (
         <div className="bg-gradient-to-br from-lyratech-purple via-[#3f416e] to-lyratech-blue pb-20">
             <Profile imageSrc={DaniQImage} name="Daniel Queijeiro Albo"/>
+            <ButtonLanguage />
             <Contact
                 phone="524423015435"
                 email="dany.queijeiro@hotmail.com"
