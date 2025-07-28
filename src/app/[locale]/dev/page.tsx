1a833e703afdebdf623fe75ed58eddecd852379d
Ricardo0919 <rickisierra03@gmail.com>
2025-07-28 14:35:27 -0600

    Footer Component

--- src/app/[locale]/dev/page.tsx
+++ src/app/[locale]/dev/page.tsx
@@ -4,15 +4,17 @@ import Navbar from "@/components/Navbar/index";
 import Hero from "@/components/Home/HeroHome";
 import HelpAndSupport from "@/components/Home/HelpAndSupport";
 import ButtonLanguage from "@/components/ButtonLanguage";
+import Footer from "@/components/Footer";
 
 
 export default function Home() {
     return (
         <div className="">
             <Navbar />
+            <ButtonLanguage />
             <Hero />
             <HelpAndSupport />
-            <ButtonLanguage />
+            <Footer />
         </div>
     );
 }
