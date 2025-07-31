a8abcb69715a1b8ce7c0cf33043b951bf954aec0
Ricardo0919 <rickisierra03@gmail.com>
2025-07-30 21:23:26 -0600

    About Us Scroll Component

--- src/app/[locale]/dev/page.tsx
+++ src/app/[locale]/dev/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-//import Image from "next/image";
 import Navbar from "@/components/Navbar/index";
 import Hero from "@/components/Home/HeroHome";
 import AboutUs from "@/components/Home/AboutUs";
