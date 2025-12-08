35ed26c7b37a23b4146664e1eb2d8dbdb2558b6c
Ricardo0919 <rickisierra03@gmail.com>
2025-12-08 01:18:35 +0100

    Upgrade version of next.js for security and deploy in netlify and added new projects to portafolio

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,23 +1,23 @@
 import clsx from 'clsx';
-import {getMessages, unstable_setRequestLocale} from 'next-intl/server';
-import {ReactNode} from 'react';
-import {locales} from '@/config';
-import "../globals.css";
-import {NextIntlClientProvider} from "next-intl";
+import { getMessages, unstable_setRequestLocale } from 'next-intl/server';
+import { ReactNode } from 'react';
+import { locales } from '@/config';
+import '../globals.css';
+import { NextIntlClientProvider } from 'next-intl';
+import type { Metadata } from 'next';
 
 type Props = {
     children: ReactNode;
     params: Promise<{ locale: string }>;
 };
 
-
 export function generateStaticParams() {
-    return locales.map((locale) => ({locale}));
+    return locales.map((locale) => ({ locale }));
 }
 
-export const dynamic = 'force-dynamic'; // 🚀 Forzar actualización en cada render
+export const dynamic = 'force-dynamic';
 
-export async function generateMetadata() {
+export async function generateMetadata(): Promise<Metadata> {
     return {
         title: "Lyra Technologies",
         description: "Lyra Tech website",
@@ -28,20 +28,15 @@ export async function generateMetadata() {
     };
 }
 
-
-
-export default async function LocaleLayout({children, params}: Props) {
-    // Enable static rendering
+export default async function LocaleLayout({ children, params }: Props) {
     const { locale } = await params;
     unstable_setRequestLocale(locale);
     const messages = await getMessages();
 
     return (
-
         <html className="h-full" lang={locale}>
-
         <body className={clsx('flex h-full flex-col')}>
-        <NextIntlClientProvider messages={messages}>
+        <NextIntlClientProvider messages={messages} locale={locale}>
             {children}
         </NextIntlClientProvider>
         </body>
