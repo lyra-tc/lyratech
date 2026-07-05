a8dbc344ead86e2be8bf99c58940c5f19a34761b
Ricardo0919 <rickisierra03@gmail.com>
2026-07-05 20:29:38 +0200

    Update go diagnostic button

--- frontend/src/components/Services/DiagnosticGo/FloatingButton.tsx
+++ frontend/src/components/Services/DiagnosticGo/FloatingButton.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import { useTranslations } from "next-intl";
-import { HiOutlineSparkles } from "react-icons/hi2";
+import { HiOutlineClipboardCheck } from "react-icons/hi";
 import DiagnosticGoModal from "./Modal";
 
 export default function DiagnosticGoFloatingButton() {
@@ -17,8 +17,8 @@ export default function DiagnosticGoFloatingButton() {
         aria-label={t("floatingButtonLabel")}
         className="fixed bottom-24 right-5 z-50 rounded-full p-[2px] bg-gradient-to-br from-lyratech-purple via-[#7b88e8] to-lyratech-blue shadow-[0_0_15px_#5f66ae] transition-transform duration-300 hover:scale-110 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-lyratech-purple"
       >
-        <div className="bg-black rounded-full w-11 h-11 flex items-center justify-center">
-          <HiOutlineSparkles className="text-white" size={22} />
+        <div className="flex items-center justify-center rounded-full bg-black p-2">
+          <HiOutlineClipboardCheck className="text-white" size={35} />
         </div>
       </button>
 
